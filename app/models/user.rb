4420a022abec9820261f8f8d9ca156a53d6507e6
seiji_okura <slower.than.snails@gmail.com>
2016-02-10 22:24:46 +0000

    add login and session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  #VALID_EMAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEXP  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50 }
+  validates :email, presence: true, length: {maximum: 255 },
+                    format: {with: VALID_EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  
 end
